b299c488477507ba1b449a6423b1fdd7f1e7997d
jvendrow <jvendrow@gmail.com>
2020-06-01 16:36:30 -0700

    some updates

--- ppo.py
+++ ppo.py
@@ -5,34 +5,46 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 import gym
 
-
 class Policy(nn.Module):
+    """
+    A simple neural network whose input is an
+    observation/state and output is the action
+    probabilities.
+    """
 
     def __init__(self, input_size, output_size):
 
         super(Policy, self).__init__()
 
-        self.fc1 = nn.Linear(input_size, 40)
-        self.fc2 = nn.Linear(40, 40)
-        self.fc3 = nn.Linear(40, output_size)
+        self.fc1 = nn.Linear(input_size, 10)
+        self.fc2 = nn.Linear(10, 10)
+        self.fc3 = nn.Linear(10, output_size)
+        self.softmax = nn.Softmax(dim=-1)
 
     def forward(self, x):
 
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
+        x = self.softmax(x)
 
         return x
 
 class Critic(nn.Module):
+    """
+    A simple neural network whose input is an
+    observation/state and output is the value.
+    """
 
     def __init__(self, input_size):
 
         super(Critic, self).__init__()
 
-        self.fc1 = nn.Linear(input_size, 40)
-        self.fc2 = nn.Linear(40, 40)
-        self.fc3 = nn.Linear(40, 1)
+        self.fc = nn.Linear(input_size, 1)
+
+        self.fc1 = nn.Linear(input_size, 10)
+        self.fc2 = nn.Linear(10, 10)
+        self.fc3 = nn.Linear(10, 1)
 
     def forward(self, x):
 
@@ -43,8 +55,13 @@ class Critic(nn.Module):
         return x
 
 
+
 class PPO():
 
+    """
+    A class that runs Proximal Policy Optimization on an Openai gym
+    envoriment. 
+    """
 
     def __init__(self, env, obs_size, act_size, eps, gamma, alpha):
 
@@ -58,15 +75,17 @@ class PPO():
         self.alpha = alpha
         self.eps = eps
 
-        self.pi = Policy(obs_size, act_size)
-        self.critic = Critic(obs_size)
 
-        self.optimizer_pi = torch.optim.Adam(self.pi.parameters(), lr=0.0005)
+        self.net = FCNet(obs_size)
+        self.pi = Policy(obs_size, act_size, self.net)
+        self.critic = Critic(obs_size, self.net)
+
+        self.optimizer_pi = torch.optim.Adam(self.pi.parameters(), lr=0.005)
         self.optimizer_critic = torch.optim.Adam(self.critic.parameters(), lr=0.005)
 
     def sample(self, pi):
         """
-        Evalulate the given policy using sampling
+        Make an state, action, reward, state observation.
         """
         observation1 = self.env.reset()
         observation1 = Variable(torch.from_numpy(observation1), requires_grad=False).detach()
@@ -84,26 +103,6 @@ class PPO():
 
         return policy(observation.float())[action.detach()]
 
-    def calculate_ratio(self, pi_new, pi_old, obs, act):
-        """
-        Calculates the probability ration between the
-        new and old policies
-
-        Parameters
-        ----------
-        pi_new: Torch Tensor
-            The new policy
-
-        pi_old: Torch Tensor
-            The old policy
-
-        Returns
-        -------
-        ratio: int
-            the probability ratio between the policies
-        
-        """
-
         ratio1 = prob(pi_new, obs, act) 
         ratio2 = prob(pi_old, obs, act)
 
@@ -112,6 +111,10 @@ class PPO():
 
     def L_CLIP_loss(self, ratio, advantage):
 
+        """
+        Calculate CLIP loss from paper
+        """
+
         loss1 = ratio * advantage
         loss2 = torch.clamp(ratio, 1-self.eps, 1+self.eps) * advantage
 
@@ -119,13 +122,19 @@ class PPO():
 
     def calculate_loss(self, policy, advantage, obs, act, prob_old):
 
+        """
+        Calcuate the total actor loss
+        """
+
         r = self.prob(policy, obs, act) / prob_old
-        
-        return self.L_CLIP_loss(r, advantage)
+        probs = self.pi(obs.float())
+        return self.L_CLIP_loss(r, advantage) - 1 * torch.mean(probs * torch.log(probs + 1e-10))
+
 
     def step(self):
-
-        self.alpha = self.alpha * 0.999
+        """
+        Take a single step of the PPO update
+        """
         #sample stata, action, reward, state
         observation1, action, reward, observation2 = self.sample(self.pi)
         observation2 = Variable(torch.from_numpy(observation2)).detach()
@@ -145,8 +154,4 @@ class PPO():
 
         self.optimizer_critic.zero_grad()
         critic_loss.backward()
-
-
-
-
 
