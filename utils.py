9a6011d8942b6319fdfc1a2a56349b9267c978d9
Rahul Malavalli <rahul.malavalli@gmail.com>
2020-06-08 01:47:27 -0700

    Added utils to save checkpoints during training

---
+++ utils.py
@@ -0,0 +1,29 @@
+import os
+from typing import Any, Dict, Text
+
+import torch
+
+from ppo import PPO
+
+
+def save_ppo_checkpoint(
+  exp_dir: Text,
+  epoch: int,
+  ppo: PPO
+) -> None:
+  os.makedirs(exp_dir, exist_ok=True)
+  checkpoint_file = os.path.join(exp_dir, "checkpoint-{}.tar".format(epoch))
+
+  checkpoint: Dict[Text, Any] = {
+    "epoch": epoch,
+    "env": str(ppo.env)
+  }
+
+  for model_name, model, model_opt in [
+    ("actor", ppo.pi, ppo.optimizer_pi),
+    ("critic", ppo.critic, ppo.optimizer_critic)
+  ]:
+    checkpoint["{}-model_state_dict"] = model.state_dict()
+    checkpoint["{}-optimizer_state_dict"] = model_opt.state_dict()
+
+  torch.save(checkpoint, checkpoint_file)
