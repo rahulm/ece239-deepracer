7fd4cf328eaf50788c8efa949aa29856cb2a1056
Rahul Malavalli <rahul.malavalli@gmail.com>
2020-06-08 02:01:19 -0700

    Fixed typo to include model name (actor vs. critic) in checkpoint

--- utils.py
+++ utils.py
@@ -23,7 +23,7 @@ def save_ppo_checkpoint(
     ("actor", ppo.pi, ppo.optimizer_pi),
     ("critic", ppo.critic, ppo.optimizer_critic)
   ]:
-    checkpoint["{}-model_state_dict"] = model.state_dict()
-    checkpoint["{}-optimizer_state_dict"] = model_opt.state_dict()
+    checkpoint["{}-model_state_dict".format(model_name)] = model.state_dict()
+    checkpoint["{}-optimizer_state_dict".format(model_name)] = model_opt.state_dict()
 
   torch.save(checkpoint, checkpoint_file)
